57151b13709024ef2aeb9c8cd92f3e50b18ea1f0
PovilasU <povilas.urbonas85@gmail.com>
2023-11-27 20:43:21 +0000

    Created Employee class

--- Tribeca.WebAPI/Tribeca.WebAPI/EnglishToDevMagicExtensions.cs
+++ Tribeca.WebAPI/Tribeca.WebAPI/EnglishToDevMagicExtensions.cs
@@ -1,14 +1,16 @@
 ﻿namespace Tribeca.WebAPI
 {
-    //Method to translate english to Dev Magic
+    
     static class EnglishToDevMagicExtensions
     {
+        //Extension method to translate english to Dev Magic
         public static string EnglishToDevMagic(this string str) 
         {
             //TODO: implement methond
          return "EnglishToDevMagic Test: " + str.ToLower();
         }
 
+        //Extension method to translate Dev Magic to english
         public static string DevMagicToEnglish(this string str)
         {
             //TODO: implement methond
---
+++ Tribeca.WebAPI/Tribeca.WebAPI/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tribeca.WebAPI.Entities
+{
+    public class Employee
+    {
+        
+        [Key] //EmployeeId will have primary key
+        public int EmployeeID { get; set; } 
+        public int ClientID { get; set; }
+        public int OfficeID { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
