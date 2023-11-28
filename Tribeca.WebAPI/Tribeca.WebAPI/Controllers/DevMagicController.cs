0c0bada5a50bed338be2fc68c45ca3bb92c9cc67
PovilasU <povilas.urbonas85@gmail.com>
2023-11-28 00:05:34 +0000

    created initial controller to return devmagic to en

---
+++ Tribeca.WebAPI/Tribeca.WebAPI/Controllers/DevMagicController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tribeca.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevMagicController : ControllerBase
+    {
+       
+        [HttpGet]      
+        public string Get()
+        {
+            return "evday agicmay isyay osay easyyay , iyay ovelay ityay ! it'syay \r\nefinitelyday otnay ayay opycay andyay astepay ofyay igpay atinlay!".DevMagicToEnglish();
+       
+        }
+
+  
+    }
+}
