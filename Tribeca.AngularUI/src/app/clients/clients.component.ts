6414c8116036d54409993693a9da977aaa409a79
PovilasU <povilas.urbonas85@gmail.com>
2023-11-28 23:34:40 +0000

    created Clients component

---
+++ Tribeca.AngularUI/src/app/clients/clients.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-clients',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      clients works!
+    </p>
+  `,
+  styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent {
+
+}
--- Tribeca.AngularUI/src/app/home/home.component.ts
+++ Tribeca.AngularUI/src/app/home/home.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ClientsComponent } from "../clients/clients.component";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ClientsComponent],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder=" filter" />
+        <button class="primary" type="buton">Search</button>
+      </form>
+      <section class="results">
+        <app-clients> </app-clients>
+      </section>
+    </section>
   `,
-  styleUrls: ['./home.component.css']
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
