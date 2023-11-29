bc4cb87512960fe1f1242e8f6b660c19770c732b
PovilasU <povilas.urbonas85@gmail.com>
2023-11-29 16:04:18 +0000

    inject client id to details component

--- Tribeca.AngularUI/src/app/details/details.component.ts
+++ Tribeca.AngularUI/src/app/details/details.component.ts
@@ -1,19 +1,23 @@
 import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
+import { ClientsService } from "../clients.service";
+import { Client } from "../client";
 
 @Component({
   selector: "app-details",
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>details works!{{ clientId }}</p> `,
+  template: ` <p>details works!{{ client?.clientId }}</p> `,
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  clientId = 0;
+  clientsService = inject(ClientsService);
+  client: Client | undefined;
 
   constructor() {
-    this.clientId = Number(this.route.snapshot.params["id"]);
+    const clientId = Number(this.route.snapshot.params["id"]);
+    this.client = this.clientsService.getClienById(clientId);
   }
 }
