a24193a6034293422f7ba68df505bf6a82d49f6a
PovilasU <povilas.urbonas85@gmail.com>
2023-11-29 10:25:59 +0000

    Added details component and routing

--- Tribeca.AngularUI/src/app/clients/clients.component.ts
+++ Tribeca.AngularUI/src/app/clients/clients.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Client } from "../client";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-clients",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
       <img class="listing-photo" src="" />
       <h2 class="listing-heading">ClientId: {{ client.clientId }}</h2>
       <p class="listing-location">Client Name: {{ client.name }}</p>
+      <a routerLink="details"> details</a>
     </section>
   `,
   styleUrls: ["./clients.component.css"],
