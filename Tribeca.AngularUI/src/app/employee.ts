4a824bdf897fcc8c9af8277fc490e85645fc21a8
PovilasU <povilas.urbonas85@gmail.com>
2023-11-29 20:50:35 +0000

    create empoyee interface

---
+++ Tribeca.AngularUI/src/app/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+  employeeId: number;
+  officeID: number;
+  clientID: number;
+  employeeName: string;
+  bio: string;
+  dateOfBirth: string;
+  starSign: string;
+  bioAsDevMagic: string;
+}
