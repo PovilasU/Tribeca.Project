7d9e767b124fc6e93947b0d9e97922949839843b
PovilasU <povilas.urbonas85@gmail.com>
2023-11-30 12:42:54 +0000

    updated star sign tests

--- Tribeca.WebAPI/Tribeca.WebAPI.Tests/DevMagicExtensionsTests.cs
+++ Tribeca.WebAPI/Tribeca.WebAPI.Tests/DevMagicExtensionsTests.cs
@@ -8,8 +8,16 @@ namespace Tribeca.WebAPI.Tests
     {
         [Theory]
         [InlineData("2023-01-25", "Aquarius")]
+        [InlineData("2023-02-25", "Pisces")]
+        [InlineData("1987-04-01", "Aries")]
         [InlineData("1990-05-15", "Taurus")]
+        [InlineData("1990-06-20", "Gemini")]
+        [InlineData("1990-08-15", "Leo")]
+        [InlineData("1990-09-15", "Virgo")]
+        [InlineData("1990-10-15", "Libra")]
         [InlineData("1987-11-03", "Scorpio")]
+        [InlineData("1987-11-30", "Sagittarius")]
+        
         // Add more test cases as needed
         public void StarSign_ReturnsCorrectSign(string birthDate, string expectedSign)
 
