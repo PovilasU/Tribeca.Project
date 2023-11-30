326479c149abcf2849bb41caabd7e17f4ffba0c2
PovilasU <povilas.urbonas85@gmail.com>
2023-11-30 12:28:13 +0000

    updated star sign methond

---
+++ Tribeca.WebAPI/Tribeca.WebAPI.Tests/DevMagicExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+using Tribeca.WebAPI.Helpers;
+
+namespace Tribeca.WebAPI.Tests
+{
+    public class DevMagicExtensionsTests
+    {
+        [Theory]
+        [InlineData("2023-01-25", "Aquarius")]
+        [InlineData("1990-05-15", "Taurus")]
+        [InlineData("1987-11-03", "Scorpio")]
+        // Add more test cases as needed
+        public void StarSign_ReturnsCorrectSign(string birthDate, string expectedSign)
+
+        {
+            // Arrange
+            string actualSign = birthDate.StarSign();
+
+            // Assert
+            Assert.Equal(expectedSign, actualSign);
+        }
+
+        [Fact]
+        public void StarSign_ThrowsExceptionForInvalidDateFormat()
+        {
+            // Arrange
+            string invalidDate = "invalid";
+
+            // Act & Assert
+            Assert.Throws<FormatException>(() => invalidDate.StarSign());
+        }
+    }
+}
--- Tribeca.WebAPI/Tribeca.WebAPI/Helpers/DevMagicExtensions.cs
+++ Tribeca.WebAPI/Tribeca.WebAPI/Helpers/DevMagicExtensions.cs
@@ -1,20 +1,62 @@
 ﻿namespace Tribeca.WebAPI.Helpers
 {
-    static class DevMagicExtensions
+    public static class DevMagicExtensions
     {
         public static string StarSign(this string birthDate)
-        {
-            DateTime date = DateTime.Parse(birthDate);
+        {            
+            DateTime parsedDate = DateTime.Parse(birthDate);
+            int month = parsedDate.Month;
+            int day = parsedDate.Day;
 
-            int birthMonth = date.Month;
+            if ((month == 3 && day >= 21) || (month == 4 && day <= 19))
+            {
+                return "Aries";
+            }
+            else if ((month == 4 && day >= 20) || (month == 5 && day <= 20))
+            {
+                return "Taurus";
+            }
+            else if ((month == 5 && day >= 21) || (month == 6 && day <= 20))
+            {
+                return "Gemini";
+            }
+            else if ((month == 6 && day >= 21) || (month == 7 && day <= 22))
+            {
+                return "Cancer";
+            }
+            else if ((month == 7 && day >= 23) || (month == 8 && day <= 22))
+            {
+                return "Leo";
+            }
+            else if ((month == 8 && day >= 23) || (month == 9 && day <= 22))
+            {
+                return "Virgo";
+            }
+            else if ((month == 9 && day >= 23) || (month == 10 && day <= 22))
+            {
+                return "Libra";
+            }
+            else if ((month == 10 && day >= 23) || (month == 11 && day <= 21))
+            {
+                return "Scorpio";
+            }
+            else if ((month == 11 && day >= 22) || (month == 12 && day <= 21))
+            {
+                return "Sagittarius";
+            }
+            else if ((month == 12 && day >= 22) || (month == 1 && day <= 19))
+            {
+                return "Capricorn";
+            }
+            else if ((month == 1 && day >= 20) || (month == 2 && day <= 18))
+            {
+                return "Aquarius";
+            }
+            else
+            {
+                return "Pisces";
+            }
 
-            string[] starSigns = {
-            "Capricorn", "Aquarius", "Pisces", "Aries",
-            "Taurus", "Gemini", "Cancer", "Leo",
-            "Virgo", "Libra", "Scorpio", "Sagittarius"
-        };
-
-            return starSigns[birthMonth - 1];
         }
 
     }
